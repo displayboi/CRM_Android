cfc2780f791247b310d9add44add5fb6d5f71c03
Dani <dancanales.r@hotmail.com>
2024-12-10 21:56:10 +0100

    grafico de pega sin dark

--- app/src/main/java/com/example/crm_bueno2/screens/HomePage.kt
+++ app/src/main/java/com/example/crm_bueno2/screens/HomePage.kt
@@ -2,22 +2,36 @@ package com.example.crm_bueno2.screens
 
 
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.crm_bueno2.R
 import com.example.crm_bueno2.components.Footer
 import com.example.crm_bueno2.components.Header
 import com.google.firebase.auth.FirebaseAuth
 
 @Composable
-fun HomePage(navController: NavController) {
+fun HomePage(navController: NavController, isDarkTheme: Boolean = isSystemInDarkTheme()) {
     val auth = FirebaseAuth.getInstance()
     val email = auth.currentUser?.email
     val username = email?.substringBefore("@") ?: "Usuario"
+
+    val imageRes = remember(isDarkTheme) {
+        if (isDarkTheme) {
+            R.drawable._024_12_10_130022 // Imagen para el modo oscuro
+        } else {
+            R.drawable._024_12_10_125756 // Imagen para el modo claro
+        }
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -37,6 +51,15 @@ fun HomePage(navController: NavController) {
                 text = "Bienvenido, $username",
                 style = MaterialTheme.typography.headlineMedium,
                 color = MaterialTheme.colorScheme.onBackground
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Image(
+                painter = painterResource(id = imageRes),
+                contentDescription = "Imagen de Bienvenida",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(400.dp) // Ajusta el tamaño según tu diseño
             )
             Spacer(modifier = Modifier.height(16.dp))
 
