25286adb48df212ef97b75f936dc693de88be89f
Dani <dancanales.r@hotmail.com>
2024-12-10 14:13:07 +0100

    colores correctos

--- app/src/main/java/com/example/crm_bueno2/ui/theme/Color.kt
+++ app/src/main/java/com/example/crm_bueno2/ui/theme/Color.kt
@@ -3,19 +3,21 @@ package com.example.crm_bueno2.ui.theme
 import androidx.compose.ui.graphics.Color
 
 // Tonos marrones para modo claro
-val LightPrimary = Color(0xFF8B4513)  // Marrón primario
-val LightSecondary = Color(0xFFD2B48C) // Marrón claro (arena)
-val LightTertiary = Color(0xFFF5DEB3)  // Trigo
-val LightBackground = Color(0xFFFFF5EE) // Fondo claro (marfil)
-val LightSurface = Color(0xFFFFE4C4)    // Beige claro
-val LightOnPrimary = Color.White        // Texto sobre marrón oscuro
-val LightOnBackground = Color.Black     // Texto sobre fondo claro
+val LightPrimary = Color(0xFFCCB878)      // Marrón claro 2 (actualizado)
+val LightSecondary = Color(0xFFCCB878)   // Marrón claro 2
+val LightTertiary = Color(0xFFBC9E53)    // Marrón claro 3
+val LightBackground = Color(0xFFEDE8D0)  // Marrón claro actualizado
+val LightSurface = Color(0xFFFFE4C4)     // Beige claro
+val LightOnPrimary = Color.White         // Texto sobre marrón oscuro
+val LightOnBackground = Color.Black      // Texto sobre fondo claro
 
 // Tonos marrones para modo oscuro
-val DarkPrimary = Color(0xFF5C4033)    // Marrón oscuro
-val DarkSecondary = Color(0xFF8B7765)  // Marrón grisáceo
-val DarkTertiary = Color(0xFF4E342E)   // Marrón chocolate oscuro
-val DarkBackground = Color(0xFF2C2C2C) // Fondo oscuro
-val DarkSurface = Color(0xFF3E2723)    // Marrón más oscuro
-val DarkOnPrimary = Color.White        // Texto sobre marrón oscuro
+val DarkPrimary = Color(0xFF65462E)      // Marrón oscuro actualizado
+val DarkSecondary = Color(0xFF785430)    // Marrón oscuro 2
+val DarkTertiary = Color(0xFF65462E)     // Marrón oscuro 3
+val DarkBackground = Color(0xFF322016)   // Marrón oscuro actualizado
+val DarkSurface = Color(0xFF3E2723)      // Marrón más oscuro
+val DarkOnPrimary = Color.White          // Texto sobre marrón oscuro
 val DarkOnBackground = Color(0xFFDCDCDC) // Texto sobre fondo oscuro
+
+
